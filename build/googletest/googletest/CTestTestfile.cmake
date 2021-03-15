9627966cde0d9b428f8138ca99b1bbb6ebe002fc
Franciszer <ftthierree@gmail.com>
2021-03-15 04:33:28 +0100

    list almost ready for testing

--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/francisco/workplace/ft_containers/googletest/googletest
-# Build directory: /home/francisco/workplace/ft_containers/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
