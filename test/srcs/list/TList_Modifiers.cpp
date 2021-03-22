9d4410f7295c2418f28ab7797c8defefefbc1f45
Franciszer <ftthierree@gmail.com>
2021-03-22 20:00:48 +0100

    all list Element Access tests done and passed

--- test/srcs/list/TList_Modifiers.cpp
+++ test/srcs/list/TList_Modifiers.cpp
@@ -15,39 +15,16 @@
 #include <list>
 #include "container_macros.hpp"
 
-// ELEMENT ACCESS TESTS
+// MODIFIER TESTS
 
 class	TList_Modifiers: public ::testing::Test {
 	public:
 		TList_Modifiers() {}
 
-		ft::list<int>		*ft_list = nullptr;
-		std::list<int>		*std_list = nullptr;
 	protected:
 		virtual void	SetUp() {}
 
 		virtual void	TearDown() {
-			if (ft_list != nullptr) {
-				delete ft_list;
-				ft_list = nullptr;
-			}
-			if (std_list != nullptr) {
-				delete std_list;
-				std_list = nullptr;
-			}
-		}
-		void		compare() {
-			if (ft_list == nullptr)
-				FAIL() << "bad_test: ft_list is uninitialized";
-			else if (std_list == nullptr)
-				FAIL() << "bad_test: std_list is uninitialized";
-			EXPECT_EQ(std_list->size(), ft_list->size());
-			ft::list<int>::const_iterator it = ft_list->begin();
-			std::list<int>::const_iterator it2 = std_list->begin();
-			ASSERT_EQ(std_list->size(), ft_list->size());
-			for (; it2 != std_list->end() ; it++, it2++)
-				EXPECT_EQ(*it, *it2);
-			this->TearDown();
 		}
 };
 
