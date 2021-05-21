0c25c86c813db1a899a6eda8046e898fd91a7985
Franciszer <ftthierree@gmail.com>
2021-05-21 18:34:36 +0200

    ...

--- test/srcs/map/TMap_Iterators.cpp
+++ test/srcs/map/TMap_Iterators.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   TMap_iterators.cpp                                 :+:      :+:    :+:   */
+/*   TMap_Iterators.cpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frthierr <frthierr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: francisco <francisco@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/21 09:43:25 by frthierr          #+#    #+#             */
-/*   Updated: 2021/05/21 09:46:48 by frthierr         ###   ########.fr       */
+/*   Updated: 2021/05/21 18:31:37 by francisco        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ class	TMap_Iterators: public ::testing::Test {
 		virtual void	TearDown() {}
 };
 
-TEST_F(TMap_Iterators, begin) {
+TEST_F(TMap_Iterators, iterators) {
 	map<int, int> m;
 	auto p = make_pair(-12321, 213321);
 	m.insert(make_pair(2,3));
@@ -43,3 +43,19 @@ TEST_F(TMap_Iterators, begin) {
 	m.insert(make_pair(414124124,2141));
 	EXPECT_TRUE(m.begin()->first == p.first && m.begin()->second == p.second);
 } 
+
+TEST_F(TMap_Iterators, reverse_iterators) {
+	map<int, int>	m;
+	pair<int, int>	p[5];
+	p[0] = make_pair(1,2);
+	p[1] = make_pair(2,3);
+	p[2] = make_pair(3,4);
+	p[3] = make_pair(4,5);
+	p[4] = make_pair(5,6);
+	for (int i = 0; i < 5 ; i++)
+		m.insert(p[i]);
+	int i = 4;
+	for (auto it = m.rbegin(); it != m.rend(); it++, i--) {
+		EXPECT_TRUE(it->first == p[i].first && it->second == p[i].second);
+	}
+}
