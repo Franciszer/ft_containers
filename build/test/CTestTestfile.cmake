864a08137d4fe18d1557c4f793f98ae1c0397fdc
Francois THIERREE <ftthierree@gmail.com>
2021-05-21 16:18:57 +0200

    erase working and tested

--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/frthierr/workplace/ft_containers/test
-# Build directory: /Users/frthierr/workplace/ft_containers/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gtest_ft_containers "/Users/frthierr/workplace/ft_containers/build/test/gtest_ft_containers")
-set_tests_properties(gtest_ft_containers PROPERTIES  _BACKTRACE_TRIPLES "/Users/frthierr/workplace/ft_containers/test/CMakeLists.txt;47;add_test;/Users/frthierr/workplace/ft_containers/test/CMakeLists.txt;0;")
